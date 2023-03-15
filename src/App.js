d079b422f7644c04d93735ae2f7de3986a95133e
ciramirez04 <cesar.ramirez.8127@miremington.edu.co>
2023-03-14 20:56:53 -0500

    se agrega proyecto

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Encabezado from './componentes';
+import Textocentrado from './componentes';
+
+function AppBar(){
+return (
+    <div className='root'>
+        <Encabezado />
+        <Textocentrado />
+    </div>
+)
+
+
+}
+
+export default Appe;
+
+
+
+
+
+
---
+++ src/componentes/Textocentrado.js
@@ -0,0 +1,21 @@
+import { Grid, Typography } from "@mui/material";
+
+export function Textocentrado() 
+    return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+<Typography variant="p">
+NUEVA COLECCIÓN 2023-V1
+XTREME
+ XTREME es ser parte de algo más grande que tú. Volver a salir y 
+reencontrarte con la montaña y con la ruta, 
+es pedalear sin descanso. Es comprender que nunca volverás a estar solo, porque eso que eres 
+es lo que nos convierte en eso que somos.
+   </Typography>>
+</Grid>
+    );
+
+
+
+   
+
+
